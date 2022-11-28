135f8633bbe0eccc81ac362be79f9df00e7e4359
Pawel Rybka <rrybka1999@gmail.com>
2022-11-28 19:10:07 +0100

    Change layout

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".burger-menu");
+const header = document.querySelector("header")
 
 burger.addEventListener("click", () => {
     menu.classList.toggle("active")
     burger.classList.toggle("active")
 })
+
+window.onscroll = function(){
+    if(window.scrollY > 22){
+        header.classList.add("headerShadow")
+    }
+    else{
+        header.classList.remove("headerShadow")
+    }
+
+}
 
