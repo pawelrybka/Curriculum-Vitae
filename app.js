e1ab88ad60db6ef656e19b36cb3aeadbd10a5aa0
Pawel Rybka <rrybka1999@gmail.com>
2022-11-30 14:53:15 +0100

    changes

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const burger = document.querySelector(".burger");
-const menu = document.querySelector(".burger-menu");
+const menu = document.querySelector(".menu");
 const header = document.querySelector("header")
+const menuLinks = document.querySelectorAll(".menu-link")
 
 burger.addEventListener("click", () => {
     menu.classList.toggle("active")
     burger.classList.toggle("active")
 })
 
-window.onscroll = function(){
-    if(window.scrollY > 22){
+function scrollHeader(){
+    if(window.scrollY > 0){
         header.classList.add("headerShadow")
     }
     else{
         header.classList.remove("headerShadow")
     }
 }
+
+window.addEventListener("scroll", scrollHeader)
+   
+menuLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+        const key = e.target.dataset.key;
+        const section = document.querySelector(`.${key}`).getBoundingClientRect().top + window.pageYOffset - 65;
+        
+        menu.classList.remove("active");
+        burger.classList.remove("active");
+
+        window.scrollTo({ top: section, behavior: "smooth" });
+    })
+});
+
 
 
